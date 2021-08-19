87a6eff820efbeed5c5162affa1a22c5becb9e1b
teityura <teityura@gmail.com>
2021-08-19 22:58:58 +0900

    Edit Sphere.cs

--- Assets/Scripts/SampleScene/Sphere.cs
+++ Assets/Scripts/SampleScene/Sphere.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class Sphere : MonoBehaviour
 {
+    Subject<string> subject = new Subject<string>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        subject.Subscribe(msg => Debug.Log("msg1: " + msg));
+        subject.OnNext("Good Morning");
+        subject.OnNext("Hello");
+
+        subject.Subscribe(msg => Debug.Log("msg2: " + msg));
+        subject.Subscribe(msg => Debug.Log("msg3: " + msg));
+        subject.OnNext("Good Evening");
     }
 
     // Update is called once per frame
